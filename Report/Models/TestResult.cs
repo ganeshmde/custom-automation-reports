058495850f66305ad1cbc3b5290745f3fe8cd128
Ganesh <90398683+ganeshmde@users.noreply.github.com>
2023-12-09 13:47:10 +0530

    Add files via upload

---
+++ Report/Models/TestResult.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomExtentReport.Report.Models
+{
+    public class TestResult
+    {
+        public int TotalScenarios { get; set; }
+
+        public int FailedScenarios { get; set; }
+
+        public double PassPercent { get; set; }
+
+        public string Duration { get; set; }
+    }
+}
